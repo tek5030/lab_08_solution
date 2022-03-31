dc0dca5ee5da26f01785119e67a89e30909bbdd5
Trym Vegard Haavardsholm <trym.haavardsholm@its.uio.no>
2022-03-31 20:41:28 +0200

    Updated with image size

--- intrinsic_calibration.h
+++ intrinsic_calibration.h
@@ -12,14 +12,16 @@ using Vec5d = Vec<double, 5>;
 class IntrinsicCalibration
 {
 public:
-  IntrinsicCalibration(cv::Matx33d K, cv::Vec5d dist_coeffs);
+  IntrinsicCalibration(cv::Matx33d K, cv::Vec5d dist_coeffs, cv::Size2i img_size);
 
   const cv::Matx33d K_cv() const;
   const Eigen::Matrix3d K() const;
   const cv::Vec5d distCoeffs() const;
+  const cv::Size2i img_size() const;
 
 private:
   cv::Matx33d K_cv_;
   cv::Vec5d dist_coeffs_;
+  cv::Size2i img_size_;
   Eigen::Matrix3d K_;
 };
